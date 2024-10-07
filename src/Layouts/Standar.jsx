73c8e9933ed426265100bddd14ef869a8e739bdc
Black <alejandrazacariasrt@gmail.com>
2024-10-07 01:14:25 -0300

    eliminando componentes innecesarios

--- src/Layouts/Standar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Standar() {
-  return (
-    <div>Standar</div>
-  )
-}
---
+++ src/Layouts/Standard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Standard() {
+  return (
+    <div>Standard</div>
+  )
+}
