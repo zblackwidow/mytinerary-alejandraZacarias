801cd86dd3bad26c1c84e3acd4f28501322dc80b
Black <alejandrazacariasrt@gmail.com>
2024-10-07 19:36:23 -0300

    resolviendo conflictos

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,18 +1,17 @@
-import React from "react";
 
 export default function Loading() {
   return (
-    <div class="shadow rounded-md p-4 my-2 max-w-sm w-full mx-auto">
-      <div class="animate-pulse flex space-x-4">
-        <div class=" bg-slate-800 h-20 w-20"></div>
-        <div class="flex-1 space-y-6 py-1">
-          <div class="h-2 bg-slate-800 rounded"></div>
-          <div class="space-y-3">
-            <div class="grid grid-cols-3 gap-4">
-              <div class="h-2 bg-slate-800 rounded col-span-2"></div>
-              <div class="h-2 bg-slate-800 rounded col-span-1"></div>
+    <div className="shadow rounded-md p-4 my-2 max-w-sm w-full mx-auto">
+      <div className="animate-pulse flex space-x-4">
+        <div className=" bg-slate-800 h-20 w-20"></div>
+        <div className="flex-1 space-y-6 py-1">
+          <div className="h-2 bg-slate-800 rounded"></div>
+          <div className="space-y-3">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-2 bg-slate-800 rounded col-span-2"></div>
+              <div className="h-2 bg-slate-800 rounded col-span-1"></div>
             </div>
-            <div class="h-2 bg-slate-800 rounded"></div>
+            <div className="h-2 bg-slate-800 rounded"></div>
           </div>
         </div>
       </div>
