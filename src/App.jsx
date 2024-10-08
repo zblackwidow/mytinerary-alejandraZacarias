df3f27211d516e57232ba131eea67b5df3c24e72
Black <alejandrazacariasrt@gmail.com>
2024-10-08 20:10:13 -0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import "./App.css";
 import Home from "./Pages/Home";
 import Cities from "./Pages/Cities";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Standard from "./Layouts/Standard";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  {element: <Standard></Standard>,
+    children: [
+      { path: "/", element: <Home /> },
+    ]
+  },
   { path: "/cities", element: <Cities /> },
 ]);
 
